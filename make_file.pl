4d2605ce9f5176516b926e030a250d87110459ef
Toby Corkindale <tobyc@strategicdata.com.au>
2010-05-20 13:03:50 +1000

    Make more complex CSV input file.
    Now that we have a proper CSV engine in every tested language,
    output CSV files that test them further - include commas and
    quotes in the input csv.

--- make_file.pl
+++ make_file.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Text::CSV;
+use feature 'say';
+
+my @ops = ('+', '-', ',', '&', '*', '"');
 
 my @names = qw(
     Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel
@@ -9,10 +13,20 @@ my @names = qw(
     Whiskey Xray Yankee Zulu
 );
 
-print "name,integer,float\n";
-for (1..10000000) {
+my $csv = Text::CSV->new;
+$csv->combine(qw(name integer float));
+say $csv->string;
+
+
+for (1..100000) {
     my $int = int(rand(99) + 1);
     my $float = rand(1);
-    my $name = $names[ int(rand(scalar(@names))) ];
-    print join(',', $name, $int, $float) . "\n";
+    my $op = $ops[int(rand(scalar(@ops)))];
+    my $name = join($op, @names[
+            int(rand(scalar(@names))),
+            int(rand(scalar(@names)))
+        ]);
+
+    $csv->combine($name, $int, $float);
+    say $csv->string;
 }
