2be03644de258310b46b344d73858a5c4b5829a2
Toby Corkindale <tobyc@strategicdata.com.au>
2010-05-21 19:12:42 +1000

    Use buffered IO for Scala test.
    (Improves time from 89 to 67 seconds on big file)

--- scala/PerfTest.scala
+++ scala/PerfTest.scala
@@ -1,8 +1,11 @@
 import scala.io.Source
 import au.com.bytecode.opencsv.CSVReader // from opencsv.sf.net
 import java.io.FileReader // for CSVReader
+import java.io.BufferedOutputStream
 
 object PerfTest {
+    val output = new BufferedOutputStream(System.out)
+
     def main(args: Array[String]) {
         if (args.length < 1) {
             println("Please pass filename as parameter.")
@@ -29,7 +32,7 @@ object PerfTest {
         do {
             val nextLine = reader.readNext()
             nextLine match {
-                case null => return
+                case null => { output.flush; return; }
                 case _ => csv_line(nextLine)
             }
         } while (true)
@@ -40,6 +43,9 @@ object PerfTest {
         val name = columns(0)
         val result = columns(1).toDouble * columns(2).toDouble
 
-        printf("%s is %.02f\n", name, result);
+        //val formatted = String.format("%s is %.02f\n", name, result)
+        val formatted = "%s is %.02f\n".format(name, result)
+        output.write(formatted.getBytes)
+        // printf("%s is %.02f\n", name, result);
     }
 }
